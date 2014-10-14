078b03068a4edcbe842f45819927c93f1b72cffb
Richard Chang <richard@vm5.com>
2014-10-14 10:21:24 +0800

    [Title]: Remove google app when pulling
    [Problem]:
    [Solution]:
    [Note]:

--- script/pull_apk.sh
+++ script/pull_apk.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 
-adb shell ls /data/app/ | grep -v android | tr -d '\015' | while read file
+adb shell ls /data/app/ | grep -v google | tr -d '\015' | while read file
 do
     adb pull /data/app/$file .
 done
